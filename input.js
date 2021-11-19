f30ebb6e0636ad9dcb751e7e2ef7c3e02c1b84f4
Amy <amymccarthy2014@gmail.com>
2021-11-19 01:06:06 +0000

    create movement commands using WASD keystrokes; update setupInput function to include connect()

--- input.js
+++ input.js
@@ -1,7 +1,10 @@
 const net = require('net');
 const { connect } = require('./client');
 
-const setupInput = function () {
+let connection;
+
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -13,6 +16,18 @@ const setupInput = function () {
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
+  }
+  if (key === '\u0077' || key === '\u0057') { // w or W
+    connection.write('Move: up');
+  }
+  if (key === '\u0061' || key === '\u0410') { // a or A
+    connection.write('Move: left');
+  }
+  if (key === '\u0073' || key === '\u0053') { // s or S
+    connection.write('Move: down');
+  }
+  if (key === '\u0064' || key === '\u0044') { // d or D
+    connection.write('Move: right');
   }
 };
 
