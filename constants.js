da6237a841403af49961e9de1c7f4fcd6dc0ad49
Amy <amymccarthy2014@gmail.com>
2021-11-19 02:10:08 +0000

    created file to contain data that is static, assigned host and port to const variables

---
+++ constants.js
@@ -0,0 +1,9 @@
+const IP = { host: 'localhost' }; // replace 'localhost' with IP address if playing with someone else
+const PORT = { port: 50541 };
+
+module.exports = {
+  IP,
+  PORT,
+};
+
+    
