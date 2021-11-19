c17c062bff2376c683da32915e805b561bfa09bb
Amy <amymccarthy2014@gmail.com>
2021-11-19 02:07:58 +0000

    removed object for host and port, added constants file containing IP and PORT

--- client.js
+++ client.js
@@ -1,10 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function () { // establish connection with the game server
-  const conn = net.createConnection({
-    host: 'localhost', // replace 'localhost' with IP address if playing with someone else
-    port: 50541
-  });
+  const conn = net.createConnection({ ...IP, ...PORT });
+  // {
+  //   host: 'localhost', // replace 'localhost' with IP address if playing with someone else
+  //   port: 50541
+  // });
 
   conn.setEncoding('utf8');
 
