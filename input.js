3421ae5c3eba8fa2ca63c08a2acddb1d89cbb760
Amy <amymccarthy2014@gmail.com>
2021-11-19 04:14:51 +0000

    remove unused code; include comments on user input commands

--- input.js
+++ input.js
@@ -1,5 +1,4 @@
 const net = require('net');
-// const { connect } = require('./client');
 
 let connection;
 
@@ -14,25 +13,25 @@ const setupInput = function (conn) {
 };
 
 const handleUserInput = function (key) {
-  if (key === '\u0003') {
+  if (key === '\u0003') { // ctrl+C to exit game
     process.exit();
   }
-  if (key === '\u0077' || key === '\u0057') { // w or W
+  if (key === '\u0077' || key === '\u0057') { // w or W to move "up"
     connection.write('Move: up');
   }
-  if (key === '\u0061' || key === '\u0041') { // a or A
+  if (key === '\u0061' || key === '\u0041') { // a or A to move "left"
     connection.write('Move: left');
   }
-  if (key === '\u0073' || key === '\u0053') { // s or S
+  if (key === '\u0073' || key === '\u0053') { // s or S to move "down"
     connection.write('Move: down');
   }
-  if (key === '\u0064' || key === '\u0044') { // d or D
+  if (key === '\u0064' || key === '\u0044') { // d or D to move "right"
     connection.write('Move: right');
   }
-  if (key === '\u0062' || key === '\u0042') { // b or B
+  if (key === '\u0062' || key === '\u0042') { // b or B to display message to other players
     connection.write('Say: I IZ DA BEST');
   }
-  if (key === '\u0068' || key === '\u0048') { // h or H
+  if (key === '\u0068' || key === '\u0048') { // h or H to display message to other players
     connection.write('Say: HAHA, 2 SLOW!!');
   }
 };
