63888c3c31619851e66aad7feb55d8247a11256b
Amy <amymccarthy2014@gmail.com>
2021-11-19 04:24:29 +0000

    remove unused code; included comments on connect function

--- client.js
+++ client.js
@@ -3,29 +3,19 @@ const { IP, PORT } = require('./constants');
 
 const connect = function () { // establish connection with the game server
   const conn = net.createConnection({ ...IP, ...PORT });
-  // {
-  //   host: 'localhost', // replace 'localhost' with IP address if playing with someone else
-  //   port: 50541
-  // });
 
   conn.setEncoding('utf8');
 
   conn.on('connect', () => {
-    conn.write('Name: AMC');
+    conn.write('Name: AMC'); // insert initials to be displayed over player's snake
     console.log('Successfully connected to game server.');
-    // conn.write('Move: up'); // play around with sending "move" commands
-    // let interval = setInterval(() => {
-    //   conn.write('Move: up');
-    // }, 100);
   });
 
   conn.on('data', (data) => {
-    console.log('Game Alert: ', data);
+    console.log('Game Alert: ', data); // displays messages from the server to the player
   });
 
   return conn;
 };
 
-module.exports = {
-  connect
-};
+module.exports = { connect };
